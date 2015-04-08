1ccb3e7e3a292b3d4e372cd93ff526a2ffef23ef
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2015-04-08 22:31:26 +0200

    First take on PoSH thingy

--- src/ReleaseCmdLet.ps1
+++ src/ReleaseCmdLet.ps1
@@ -5,17 +5,20 @@ $organizationId = '42173c5126aa4fdf8acdf368c8555f7c'
 $sha1 = 'D5E5FDD40F607787FE57486226647A7C770C4A1D'
 
 #https://opbeat.com/api/v1/organizations/<organization-id>/apps/<app-id>/releases/
-$uri = 'https://opbeat.com/api/v1/organizations/{$organizationId}/apps/{$applicationid}/releases/'
+$uri = "https://opbeat.com/api/v1/organizations/$organizationId/apps/$applicationid/releases/"
 
 $userAgent = 'opbeat.net/1.0'
 
 $headers = @{
-	Authorization = 'Bearer {$accessToken}'
+	'Authorization' = "Bearer $accessToken"
+	'Content-Type' = 'application/json'
 }
 
-$content = @{
-	rev = '{$sha1}'
+$body = ConvertTo-Json @{
+	rev = "$sha1"
 	status = 'completed'
 }
 
-Invoke-RestMethod -Method Post -Headers $headers -Uri $uri -UserAgent $userAgent -ContentType 'application/json' -Content $content
+$result = Invoke-RestMethod -Method Post -Headers $headers -Uri $uri -UserAgent $userAgent -Body $body
+
+Write-Host $result
