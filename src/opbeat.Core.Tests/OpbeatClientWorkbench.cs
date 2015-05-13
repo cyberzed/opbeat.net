0a62c7d01904d872ee310f3fcb612913181a85fc
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2015-05-13 14:29:53 +0200

    Added first sample of pushing to error channel

--- src/opbeat.Core.Tests/OpbeatClientWorkbench.cs
+++ src/opbeat.Core.Tests/OpbeatClientWorkbench.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using opbeat.Core.ErrorsModels;
 using opbeat.Core.ReleaseModels;
 using Xunit;
+using Exception = opbeat.Core.ErrorsModels.Exception;
 
 namespace opbeat.Core.Tests
 {
@@ -28,6 +29,25 @@ namespace opbeat.Core.Tests
         }
 
         [Fact]
+        public void SendTestError()
+        {
+            var configuration = new OpbeatConfiguration
+            {
+                AccessToken = "0d85cde89fadf8d18c057dd9572b6a2579aaa85e",
+                ApplicationId = "554e269e6f",
+                OrganizationId = "42173c5126aa4fdf8acdf368c8555f7c"
+            };
+
+            var client = new OpbeatClient(configuration);
+
+            var error = new Error("test");
+
+            var response = client.Send(error);
+
+            Assert.Equal(ServiceResponse.Success, response);
+        }
+
+        [Fact]
         public void SerializeRelease()
         {
             var release = new Release(Sha1Generator.RandomString(), ReleaseStatus.MachineCompleted)
@@ -42,17 +62,31 @@ namespace opbeat.Core.Tests
         [Fact]
         public void SerializeError()
         {
-            var release = new Error("test")
+            var error = new Error("Input 42")
             {
-                Timestamp = DateTime.UtcNow,
+                Culprint = "opbeat.Core.Tests.OpbeatClientWorkbench.SerilizeError",
+                Exception = new Exception
+                {
+                    Value = "test",
+                    Module = "opbeat.Core.Tests",
+                    Type = "UnitTest"
+                },
+                Extra = new Dictionary<string, string> {{"foo", "bar"}},
+                Http = new Http(),
+                Level = ErrorLevel.Fatal,
+                Logger = "test",
                 Machine = new Dictionary<string, string>
                 {
                     {"hostname", Environment.MachineName},
                     {"UserDomainName", Environment.UserDomainName}
-                }
+                },
+                Param_Message = "Input {0}",
+                StackTrace = new StackTrace(),
+                Timestamp = DateTime.UtcNow,
+                User = new Dictionary<string, string> {{"horse", "man"}}
             };
 
-            var output = Serializer.Serialize(release);
+            var output = Serializer.Serialize(error);
         }
     }
 }
