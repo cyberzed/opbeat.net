30f43b06deccaf642dd8086e1b0e9ed91d480298
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2015-06-22 22:20:01 +0200

    Stronger implementation for StackTrace

--- src/opbeat.Core/ErrorsModels/StackTrace.cs
+++ src/opbeat.Core/ErrorsModels/StackTrace.cs
@@ -1,9 +1,41 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace opbeat.Core.ErrorsModels
 {
     public class StackTrace
     {
-        public List<Frame> Frames { get; set; }
+        private readonly List<Frame> frames;
+
+        public IReadOnlyList<Frame> Frames
+        {
+            get { return !frames.Any() ? null : frames.AsReadOnly(); }
+        }
+
+        public StackTrace()
+        {
+            frames = new List<Frame>();
+        }
+
+        public StackTrace(IEnumerable<Frame> frames)
+        {
+            if (frames == null)
+            {
+                throw new ArgumentNullException("frames");
+            }
+
+            this.frames = new List<Frame>(frames);
+        }
+
+        public void AddFrame(Frame frame)
+        {
+            if (frame == null)
+            {
+                throw new ArgumentNullException("frame");
+            }
+
+            frames.Add(frame);
+        }
     }
 }
