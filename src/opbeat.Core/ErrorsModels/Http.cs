0bc9074f112bc294623b65221935dc640af1e95c
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2015-07-01 21:16:08 +0200

    Basics seems implemented now

--- src/opbeat.Core/ErrorsModels/Http.cs
+++ src/opbeat.Core/ErrorsModels/Http.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 
 namespace opbeat.Core.ErrorsModels
 {
     public class Http
     {
+        private readonly IDictionary<string, string> environment;
         private readonly IDictionary<string, string> headers;
         public Uri Url { get; private set; }
         public HttpMethod Method { get; private set; }
@@ -20,18 +22,37 @@ namespace opbeat.Core.ErrorsModels
             get { return headers.Any() ? new ReadOnlyDictionary<string, string>(headers) : null; }
         }
 
-        public string RemoteHost { get; set; }
-        public string HttpHost { get; set; }
-        public string UserAgent { get; set; }
-        public bool Secure { get; set; }
-        public Dictionary<string, string> Environment { get; set; }
+        public IPAddress RemoteHost { get; private set; }
+
+        public string HttpHost
+        {
+            get { return Url.Host; }
+        }
+
+        public string UserAgent { get; private set; }
+
+        public bool Secure
+        {
+            get { return Url.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase); }
+        }
+
+        public IReadOnlyDictionary<string, string> Environment
+        {
+            get { return environment.Any() ? new ReadOnlyDictionary<string, string>(environment) : null; }
+        }
 
         public Http(Uri url, HttpMethod method)
         {
+            if (!url.IsAbsoluteUri)
+            {
+                throw new ArgumentException("Url must be absolute", "url");
+            }
+
             Url = url;
             Method = method;
 
             headers = new Dictionary<string, string>();
+            environment = new Dictionary<string, string>();
         }
 
         public void SetRequestBody(string body)
@@ -42,6 +63,41 @@ namespace opbeat.Core.ErrorsModels
         public void SetQueryString(string queryString)
         {
             QueryString = queryString;
+        }
+
+        public void SetCookies(string cookies)
+        {
+            Cookies = cookies;
+        }
+
+        public void AddHeader(string key, string value)
+        {
+            if (headers.ContainsKey(key))
+            {
+                headers.Remove(key);
+            }
+
+            headers.Add(key, value);
+        }
+
+        public void AddRemoteHost(IPAddress address)
+        {
+            RemoteHost = address;
+        }
+
+        public void AddUserAgent(string userAgent)
+        {
+            UserAgent = userAgent;
+        }
+
+        public void AddEnvironment(string key, string value)
+        {
+            if (environment.ContainsKey(key))
+            {
+                environment.Remove(key);
+            }
+
+            environment.Add(key, value);
         }
     }
 }
